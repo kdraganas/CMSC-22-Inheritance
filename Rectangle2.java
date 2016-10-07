5bc12dcd814c70a8fae0b989e1c180f3f1918686
kdraganas <kdraganas.kr@gmail.com>
2016-10-07 11:58:53 +0800

    Add files via upload

---
+++ Rectangle2.java
@@ -0,0 +1,41 @@
+public class Rectangle2 extends Shape2{
+	
+	protected double width;
+	protected double length;
+
+	public Rectangle2(){
+		width = 1.0;
+		length = 1.0;
+	}
+	public Rectangle2(double width, double length){
+		this.width = width;
+		this.length = length;
+	}
+	public Rectangle2(double width, double length, String color, boolean filled){
+		super(color,filled);
+		this.width = width;
+		this.length = length;
+	}
+	public double getWidth(){
+		return width;
+	}
+	public void setWidth(double width){
+		this.width = width;
+	}
+	public double getLength(){
+		return length;
+	}
+	public void setLength(double length){
+		this.length = length;
+	}
+	public double getArea(){
+		return width * length;
+	}
+	public double getPerimeter(){
+		return ((2*width) + (2*length));
+	}
+	@Override
+	public String toString(){
+		return "A Rectangle with width = " + width + " and length = " + length + " which is a subclass of " + super.toString();
+	}
+}
---
+++ Square2.java
@@ -0,0 +1,29 @@
+public class Square2 extends Rectangle2{
+	
+	public Square2(){
+	}
+
+	public Square2(double side) {
+   		super(side, side); 
+	}
+	public Square2(double side, String color, boolean filled){
+		super(side, side, color, filled);
+	}
+	public double getSide(){
+		return super.getWidth();
+	}
+	public void setSide(double side){
+		setWidth(side);
+		setLength(side);
+	}
+	public void setWidth(double side){
+		super.setWidth(side);
+	}
+	public void setLength(double side){
+		super.setLength(side);
+	}
+	public String toString(){
+		return "A square with side = " + getSide() + " which is a subclass of " + super.toString();
+	}		
+
+}
