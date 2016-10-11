8189617e576d9b42fc19e7100874e78a1cada047
kdraganas <kdraganas.kr@gmail.com>
2016-10-11 15:04:42 +0800

    Create Circle2.java

---
+++ Circle2.java
@@ -0,0 +1,32 @@
+public class Circle2 extends Shape2{
+	protected double radius;
+
+	public Circle2(){
+		radius = 1.0;
+	}
+	public Circle2(double radius){
+		this.radius = radius;
+	}
+	public Circle2(double radius, String color, boolean filled){
+		super(color,filled);
+		this.radius = radius;
+	}
+	public double getRadius(){
+		return radius;
+	}
+	public void setRadius(double radius){
+		this.radius = radius;
+	}
+	@Override
+	public double getArea(){
+		return ((radius*radius) * 3.14159265359);
+	}
+	@Override
+	public double getPerimeter(){
+		return ((2*3.14159265359) * radius);
+	}
+	@Override
+	public String toString(){
+		return "A Circle with radius " + getRadius() + " which is a subclass of " + super.toString();
+	}
+}
